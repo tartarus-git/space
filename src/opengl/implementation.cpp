d65a3776f038a92e692a63ee1e37f76f6ea06715
tartarus-git <niktedig@gmail.com>
2023-04-17 21:34:10 +0200

    Made something. Haven't tested yet. Hopefully it works.

--- src/opengl/implementation.cpp
+++ src/opengl/implementation.cpp
@@ -15,3 +15,39 @@
 // NOTE: This isn't a problem though because I'll simply only use functions that I know my context version definitely supports.
 
 // TODO: Member detector idiom for detecting that second function?
+
+template <typename T>
+using trash_bool_t = bool;
+
+class does_glXGetProcAddress_exist {
+	using no  = char(&)[1];
+	using yes = char(&)[2];
+
+	template <trash_bool_t<decltype(glXGetProcAddress)> = true>
+		// TODO: Write not about how the above works.
+	consteval yes filter(void*);
+
+	consteval no filter(...);
+
+public:
+	consteval operator bool() const { return sizeof(filter(nullptr)) == sizeof(yes); }
+};
+
+// TODO: Checks if the func is in the header, but not if the symbol can be linked to. Is the latter even possible in C++?
+#define GEN_IMPL(func_name, ret_type, invocation_args, ...) ret_type APIENTRY func_name(__VA_ARGS__) { \
+	static ret_type (APIENTRY *ptr)(__VA_ARGS__) = nullptr; \
+	if (!ptr) { \
+		ptr = (decltype(ptr))glXGetProcAddressARB(#func_name); \
+		if (!ptr) { \
+			if constexpr (does_glXGetProcAddress_exst{}) { \
+				ptr = (decltype(ptr))glXGetProcAddress(#func_name); \
+				if (ptr) { return ptr ## invocation_args; } \
+			} \
+			debug::logger << "[ERROR]: function bind for " << #func_name << " failed\n"; \
+			exit_program(EXIT_FAILURE); \
+		} \
+	} \
+	return ptr ## invocation_args; /* This works even if ptr() doesn't return anything because of language. */\
+}
+
+GEN_IMPL(glUseProgram, void, (program), GLuint program)
